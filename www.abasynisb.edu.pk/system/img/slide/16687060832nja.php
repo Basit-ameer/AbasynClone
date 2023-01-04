30396313b3fc7a1184a12f56d2f80b6e15e6c4d7
BasitAmeer <36634392+Basit-ameer@users.noreply.github.com>
2023-01-05 03:15:10 +0500

    done

---
+++ www.abasynisb.edu.pk/system/img/slide/16687060832nja.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL was not found on this server.</p>
+<hr>
+<address>Apache/2.4.46 (Win64) OpenSSL/1.1.1h PHP/8.0.1 Server at www.abasynisb.edu.pk Port 80</address>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon='{"rayId":"784734936c46c8f4","token":"9071c98f049e4a44a9dfebbcfbcdd52f","version":"2022.11.3","si":100}' crossorigin="anonymous"></script>
+</body></html>
